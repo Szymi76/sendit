5f1c494419beee4bdf1871e9685ce758edc6d57d
Szymi76 <87788260+Szymi76@users.noreply.github.com>
2023-03-17 02:08:23 +0100

    Dodanie strony 404

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { Link as RouterLink } from "react-router-dom";
+import { Box, Link, Typography, styled } from "@mui/material";
+import useAuth from "../../firebase/hooks/useAuth";
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  const auth = useAuth();
+
+  const linkText = auth.user ? "Wróć do czatu" : "Wróć do logowania";
+  const linkTo = auth.user ? "/chat" : "/zaloguj-sie";
+
+  return (
+    <Wrapper>
+      <Typography variant="h2" fontWeight={400}>
+        Nie znaleziono strony
+      </Typography>
+      <Link fontSize={22} component={RouterLink} to={linkTo}>
+        {linkText}
+      </Link>
+    </Wrapper>
+  );
 };
 
 export default NotFound;
+
+const Wrapper = styled(Box)(({ theme }) => ({
+  height: "100vh",
+  width: "100%",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  gap: theme.spacing(2),
+  backgroundColor: theme.palette.grey[100],
+  padding: theme.spacing(5),
+}));
